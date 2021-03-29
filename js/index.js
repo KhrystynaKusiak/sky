e33b029fd567346a66180cef356bf5e7a80c0163
KhrystynaKusiak <khkusiak@gmail.com>
2021-03-29 21:29:21 +0000

    final_V5

--- js/index.js
+++ js/index.js
@@ -9,18 +9,14 @@ const carouselImages = ['images/slider-fre-fly.jpg', 'images/slider-kitesurfing.
 let currentIdx = 0;
 
 function showCurrentImage() {
-
     const imgContainer1 = document.querySelector('.about-services .image-first');
     const imgContainer2 = document.querySelector('.about-services .image-second');
-    const imgContainer3 = document.querySelector('.about-services .image-thirth');
-    const imgContainer4 = document.querySelector('.about-services .image-fourth');
+   
     const img2Idx = currentIdx + 1 > carouselImages.length - 1 ? 0 : currentIdx + 1;
-    const img3Idx = img2Idx + 1 > carouselImages.length - 1 ? 0 : img2Idx + 1;
-    const img4Idx = img3Idx + 1 > carouselImages.length - 1 ? 0 : img3Idx + 1;
+    
     imgContainer1.src = carouselImages[currentIdx];
     imgContainer2.src = carouselImages[img2Idx];
-    imgContainer3.src = carouselImages[img3Idx];
-    imgContainer4.src = carouselImages[img4Idx]
+   
 }
 
 function next() {
@@ -44,4 +40,42 @@ setInterval(next, 2000);
 document.querySelector('.carousel-discription-of-services .next').addEventListener('click', next);
 
 document.querySelector('.carousel-discription-of-services .prev').addEventListener('click', prev);
+
+
+const carouselImagesSmall = ['images/slider-fre-fly.jpg', 'images/slider-kitesurfing.jpg', 'images/slider-paragliding.jpg', 'images/slider-wingsuit.jpg', ]
+
+let currentSmallIdx = 0;
+
+function showCurrentImageSmall() {
+    const imgSmallContainer1 = document.querySelector('.about-services-small .image-first');
+    
+    const imgSmall2Idx = currentSmallIdx + 1 > carouselImagesSmall.length - 1 ? 0 : currentSmallIdx + 1;
+    
+    imgSmallContainer1.src = carouselImagesSmall[currentIdx];
+    
+   
+}
+
+function nextSmall() {
+    currentSmallIdx++;
+    if (currentSmallIdx > carouselImagesSmall.length - 1)
+        currentSmallIdx = 0;
+    showCurrentImageSmall();
+}
+
+function prevSmall() {
+    currentSmallIdx--;
+    if (currentSmallIdx < 0)
+        currentSmallIdx = carouselImagesSmall.length - 1;
+    showCurrentImageSmall();
+}
+
+showCurrentImageSmall();
+
+setInterval(nextSmall, 2000);
+
+document.querySelector('.carousel-discription-of-services-small .next').addEventListener('click', nextSmall);
+
+document.querySelector('.carousel-discription-of-services-small .prev').addEventListener('click', prevSmall);
+
 
